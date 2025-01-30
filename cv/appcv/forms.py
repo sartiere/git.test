457d05b84c87a3be01bd58bb2d6de7ea5a5e2204
sartiere <sartierediarra4@gmail.com>
2025-01-30 11:31:51 +0100

    First Commit

---
+++ cv/appcv/forms.py
@@ -0,0 +1,118 @@
+# forms.py
+from django import forms
+from .models import Personne, Experience, Formation, Competence, Langue, Contact, Loisir, CV
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+# Formulaire personnalisé pour l'inscription utilisateur
+class CustomUserCreationForm(UserCreationForm):
+    email = forms.EmailField(
+        required=True,
+        label="Adresse e-mail",
+        help_text="Veuillez entrer une adresse e-mail valide.",
+    )
+    password1 = forms.CharField(
+        label="Mot de passe",
+        widget=forms.PasswordInput,
+        help_text="Votre mot de passe doit contenir au moins 4 caractères.",
+    )
+    password2 = forms.CharField(
+        label="Confirmation du mot de passe",
+        widget=forms.PasswordInput,
+        help_text="Veuillez confirmer votre mot de passe.",
+    )
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
+
+    def clean_password1(self):
+        password1 = self.cleaned_data.get('password1')
+        if len(password1) < 4:
+            raise forms.ValidationError("Le mot de passe doit contenir au moins 4 caractères.")
+        return password1
+
+class PersonneForm(forms.ModelForm):
+    class Meta:
+        model = Personne
+        fields = ['nom', 'prenom', 'date_naissance', 'photo']
+
+class ExperienceForm(forms.ModelForm):
+    class Meta:
+        model = Experience
+        fields = ['titre', 'entreprise', 'date_debut', 'date_fin', 'description']
+
+class FormationForm(forms.ModelForm):
+    class Meta:
+        model = Formation
+        fields = ['diplome', 'institution', 'date_debut', 'date_fin', 'description']
+
+class CompetenceForm(forms.ModelForm):
+    class Meta:
+        model = Competence
+        fields = ['nom', 'niveau']
+
+class LangueForm(forms.ModelForm):
+    class Meta:
+        model = Langue
+        fields = ['langue', 'niveau']
+
+class ContactForm(forms.ModelForm):
+    class Meta:
+        model = Contact
+        fields = ['email', 'telephone', 'adresse']
+
+class LoisirForm(forms.ModelForm):
+    class Meta:
+        model = Loisir
+        fields = ['nom', 'description']
+
+class CVForm(forms.ModelForm):
+    DESIGN_CHOICES = [
+        ('design1', 'Design 1'),
+        ('design2', 'Design 2'),
+        ('design3', 'Design 3'),
+        ('design4', 'Design 4'),
+    ]
+    design = forms.ChoiceField(choices=DESIGN_CHOICES, required=True)
+    experiences = forms.ModelMultipleChoiceField(
+        queryset=Experience.objects.none(),
+        widget=forms.CheckboxSelectMultiple,
+        required=False
+    )
+    competences = forms.ModelMultipleChoiceField(
+        queryset=Competence.objects.none(),
+        widget=forms.CheckboxSelectMultiple,
+        required=False
+    )
+    formations = forms.ModelMultipleChoiceField(
+        queryset=Formation.objects.none(),
+        widget=forms.CheckboxSelectMultiple,
+        required=False
+    )
+    langues = forms.ModelMultipleChoiceField(
+        queryset=Langue.objects.none(),
+        widget=forms.CheckboxSelectMultiple,
+        required=False
+    )
+    loisirs = forms.ModelMultipleChoiceField(
+        queryset=Loisir.objects.none(),
+        widget=forms.CheckboxSelectMultiple,
+        required=False
+    )
+
+    class Meta:
+        model = CV
+        fields = ["personne", "contact", "experiences", "competences", "formations", "langues", "loisirs", "title", "design"]
+
+    def __init__(self, *args, **kwargs):
+        user = kwargs.pop('user', None)  # Récupérer l'utilisateur
+        super().__init__(*args, **kwargs)
+        if user:
+            # Filtrer les choix pour les relations ManyToMany ou ForeignKey en fonction de l'utilisateur
+            self.fields['personne'].queryset = Personne.objects.filter(user=user)
+            self.fields['contact'].queryset = Contact.objects.filter(user=user)
+            self.fields['experiences'].queryset = Experience.objects.filter(user=user)
+            self.fields['formations'].queryset = Formation.objects.filter(user=user)
+            self.fields['competences'].queryset = Competence.objects.filter(user=user)
+            self.fields['langues'].queryset = Langue.objects.filter(user=user)
+            self.fields['loisirs'].queryset = Loisir.objects.filter(user=user)
